ee6228da4e80361292caca4b9bd46144cd86b7bd
David Parr <dvdprr6@gmail.com>
2019-01-29 20:10:24 -0500

    run as scala program

--- install/generateGBFSDataset.sh
+++ install/generateGBFSDataset.sh
@@ -3,4 +3,7 @@
 # FOR DEBUGGING
 #export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5000
 
-spark-submit --class com.david.GenerateGBFS.main.Main --master local[*] GenerateGBFSDataset.jar
+# FOR SPARK
+#spark-submit --class com.david.GenerateGBFS.main.Main --master local[*] GenerateGBFSDataset.jar
+
+/home/vagrant/scala-2.12.8/bin/scala GenerateGBFSDataset.jar
