4f2b054a9afcfa816d00763560db60c9191dba5e
David Parr <dvdprr6@gmail.com>
2019-02-03 12:07:31 -0500

    get request for google api geocoding

--- GBFSCron/src/main/java/com/david/GBFSCron/utils/httpclient/HttpConnectionUtil.java
+++ GBFSCron/src/main/java/com/david/GBFSCron/utils/httpclient/HttpConnectionUtil.java
@@ -5,9 +5,12 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.util.EntityUtils;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.List;
 
 public class HttpConnectionUtil implements IHttpConnectionUtil, Closeable {
     private IHttpClientFactory httpClientFactory;
@@ -19,14 +22,34 @@ public class HttpConnectionUtil implements IHttpConnectionUtil, Closeable {
     @Override
     public String sendHttpGet(String url) {
         CloseableHttpResponse execute = null;
-        String jsonResponseBody = new String();
+        String jsonResponseBody = null;
 
         try {
             HttpGet httpGet = new HttpGet(url);
             httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
             execute = httpClientFactory.getHttpClient().execute(httpGet);
             jsonResponseBody = extractResponseBody(execute);
+            EntityUtils.consume(execute.getEntity());
         }catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return jsonResponseBody;
+    }
+
+    @Override
+    public String sendHttpGet(String url, List<BasicNameValuePair> body) {
+        CloseableHttpResponse execute = null;
+        String jsonResponseBody = null;
+
+        try{
+            String paramString = URLEncodedUtils.format(body, "utf-8");
+            HttpGet httpGet = new HttpGet(url + "?" + paramString);
+            httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+            execute = httpClientFactory.getHttpClient().execute(httpGet);
+            jsonResponseBody = extractResponseBody(execute);
+            EntityUtils.consume(execute.getEntity());
+        }catch(IOException ioe){
             ioe.printStackTrace();
         }
 
--- GBFSCron/src/main/java/com/david/GBFSCron/utils/httpclient/IHttpConnectionUtil.java
+++ GBFSCron/src/main/java/com/david/GBFSCron/utils/httpclient/IHttpConnectionUtil.java
@@ -1,5 +1,8 @@
 package com.david.GBFSCron.utils.httpclient;
 
+import java.util.List;
+
 public interface IHttpConnectionUtil {
     public String sendHttpGet(String url);
+    public String sendHttpGet(String url, List<BasicNameValuePair> body);
 }
