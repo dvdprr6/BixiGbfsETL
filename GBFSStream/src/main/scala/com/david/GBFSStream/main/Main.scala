c32ffc00b27bb5fa33145946e4ef6bc020a7213e
David Parr <dvdprr6@gmail.com>
2019-02-09 12:06:47 -0500

    GBFSStream

---
+++ GBFSStream/src/main/scala/com/david/GBFSStream/main/Main.scala
@@ -0,0 +1,9 @@
+package com.david.GBFSStream.main
+
+/*
+  SPARK KAFKA REFERENCE: https://spark.apache.org/docs/2.2.0/structured-streaming-kafka-integration.html
+*/
+
+object Main extends App{
+
+}
---
+++ GBFSStream/src/main/scala/com/david/GBFSStream/utils/Constants.scala
@@ -0,0 +1,13 @@
+package com.david.GBFSStream.utils
+
+object Constants {
+  val APP_NAME = "GBFSStream"
+
+  val BOOTSTRAP_SERVERS = "localhost:9092"
+
+  val KAFKA_FORMAT = "kafka"
+
+  val KAFKA_TOPIC = "trip_history"
+
+  val STREAMING_INTERVAL = 5
+}
