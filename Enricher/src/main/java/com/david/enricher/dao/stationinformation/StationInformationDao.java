405f48146d948924a94bb36515b377e702634ba0
David Parr <dvdprr6@gmail.com>
2019-02-13 21:18:25 -0500

    update the models in the AvroRegistrySchema

--- Enricher/src/main/java/com/david/enricher/dao/stationinformation/StationInformationDao.java
+++ Enricher/src/main/java/com/david/enricher/dao/stationinformation/StationInformationDao.java
@@ -1,4 +1,9 @@
 package com.david.enricher.dao.stationinformation;
 
+import com.david.avro.StationInformation;
+
+import java.util.List;
+
 public interface StationInformationDao {
+    public List<StationInformation> getById(String id);
 }
--- Enricher/src/main/java/com/david/enricher/dao/stationinformation/StationInformationDaoImpl.java
+++ Enricher/src/main/java/com/david/enricher/dao/stationinformation/StationInformationDaoImpl.java
@@ -1,4 +1,41 @@
 package com.david.enricher.dao.stationinformation;
 
+import com.david.avro.StationInformation;
+import com.david.enricher.connection.HBaseConnection;
+import com.david.enricher.utils.Constants;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.List;
+
 public class StationInformationDaoImpl implements StationInformationDao {
+    private Table table;
+
+    StationInformationDaoImpl(){
+        try{
+            this.table = HBaseConnection.getHBaseConnection().getTable(TableName.valueOf(Constants.HBASE_BIXI_GBFS_TABLE));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<StationInformation> getById(String id) {
+        Result result = null;
+
+        Get get = new Get(Bytes.toBytes(id));
+
+        try{
+            result = table.get(get);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
 }
