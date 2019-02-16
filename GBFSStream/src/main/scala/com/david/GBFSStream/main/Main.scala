4e5966a692c9eb76678216a4efc1de18651db3ae
David Parr <dvdprr6@gmail.com>
2019-02-16 13:22:43 -0500

    don't need

--- GBFSStream/src/main/scala/com/david/GBFSStream/main/Main.scala
+++
@@ -1,9 +0,0 @@
-package com.david.GBFSStream.main
-
-/*
-  SPARK KAFKA REFERENCE: https://spark.apache.org/docs/2.2.0/structured-streaming-kafka-integration.html
-*/
-
-object Main extends App{
-
-}
